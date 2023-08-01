cd35f50c58aa0eb09b8780f14b749886a19ee626
WilliamKwokX <1425183716@qq.com>
2023-08-01 16:09:09 +0800

    +

--- api/src/main/java/pers/shawxingkwok/androidutil/view/withView.kt
+++ api/src/main/java/pers/shawxingkwok/androidutil/view/withView.kt
@@ -4,6 +4,7 @@ import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import pers.shawxingkwok.ktutil.KReadOnlyProperty
 import pers.shawxingkwok.ktutil.KReadWriteProperty
 import java.lang.Exception
 import kotlin.reflect.KProperty
@@ -14,8 +15,8 @@ private object UNINITIALIZED
  * See [docs](https://shawxingkwok.github.io/ITWorks/docs/android/util-view/#withview).
  */
 @Suppress("UnusedReceiverParameter")
-public fun <T> Fragment.withView(initialize: () -> T): KReadWriteProperty<Fragment, T> =
-    object : KReadWriteProperty<Fragment, T>{
+public fun <T> Fragment.withView(initialize: () -> T): KReadOnlyProperty<Fragment, T> =
+    object : KReadOnlyProperty<Fragment, T>{
         var t: Any? = UNINITIALIZED
 
         override fun onDelegate(thisRef: Fragment, property: KProperty<*>) {
@@ -31,31 +32,21 @@ public fun <T> Fragment.withView(initialize: () -> T): KReadWriteProperty<Fragme
             }
         }
 
-        fun requireRightState(thisRef: Fragment, property: KProperty<*>){
-            // permitted before onCreateView
-            thisRef.viewLifecycleOwnerLiveData.value ?: return
-
-            // forbidden after onDestroyView
-            try {
-                thisRef.viewLifecycleOwner
-            }catch (e: Exception){
-                error(
-                    "Can't access ${thisRef.javaClass.canonicalName}.${property.name} after onDestroyView()"
-                )
-            }
-        }
-
         override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-            requireRightState(thisRef, property)
+            if (thisRef.viewLifecycleOwnerLiveData.value != null)
+                // forbidden after onDestroyView
+                try {
+                    thisRef.viewLifecycleOwner
+                }catch (e: Exception){
+                    error(
+                        "Can't access ${thisRef.javaClass.canonicalName}.${property.name} after onDestroyView()."
+                    )
+                }
+
             if (t === UNINITIALIZED) t = initialize()
 
             @Suppress("UNCHECKED_CAST")
             return t as T
-        }
-
-        override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
-            requireRightState(thisRef, property)
-            t = value
         }
     }
 
@@ -63,8 +54,8 @@ public fun <T> Fragment.withView(initialize: () -> T): KReadWriteProperty<Fragme
  * See [docs](https://shawxingkwok.github.io/ITWorks/docs/android/util-view/#withview).
  */
 @Suppress("UnusedReceiverParameter")
-public fun <T> ComponentActivity.withView(initialize: () -> T): KReadWriteProperty<ComponentActivity, T> =
-    object : KReadWriteProperty<ComponentActivity, T>{
+public fun <T> ComponentActivity.withView(initialize: () -> T): KReadOnlyProperty<ComponentActivity, T> =
+    object : KReadOnlyProperty<ComponentActivity, T>{
         var t: Any? = UNINITIALIZED
 
         override fun onDelegate(thisRef: ComponentActivity, property: KProperty<*>) {
@@ -82,9 +73,5 @@ public fun <T> ComponentActivity.withView(initialize: () -> T): KReadWriteProper
 
             @Suppress("UNCHECKED_CAST")
             return t as T
-        }
-
-        override fun setValue(thisRef: ComponentActivity, property: KProperty<*>, value: T) {
-            t = value
         }
     }
