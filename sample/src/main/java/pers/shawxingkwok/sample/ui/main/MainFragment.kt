3c1cd53fc7c5701a9d7b2f9cf683374da1416d5e
WilliamKwokX <1425183716@qq.com>
2023-08-03 19:51:23 +0800

    +

--- sample/src/main/java/pers/shawxingkwok/sample/ui/main/MainFragment.kt
+++ sample/src/main/java/pers/shawxingkwok/sample/ui/main/MainFragment.kt
@@ -1,31 +1,28 @@
 package pers.shawxingkwok.sample.ui.main
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dylanc.viewbinding.nonreflection.binding
 import pers.shawxingkwok.androidutil.view.collectOnResume
 import pers.shawxingkwok.androidutil.view.onClick
-import pers.shawxingkwok.androidutil.view.withView
+import pers.shawxingkwok.ktutil.lazyFast
 import pers.shawxingkwok.sample.R
 import pers.shawxingkwok.sample.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     private val binding by binding(FragmentMainBinding::bind)
     private val vm: MainViewModel by viewModels()
-    private val msgAdapter by withView(::MsgAdapter)
+    private val msgAdapter by lazyFast(::MsgAdapter)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.rv.run {
-            layoutManager = LinearLayoutManager(requireContext())
             adapter = msgAdapter
+            layoutManager = LinearLayoutManager(requireContext())
         }
 
         vm.msgsFlow.collectOnResume{
